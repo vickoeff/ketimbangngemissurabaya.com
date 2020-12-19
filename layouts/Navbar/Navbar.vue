74a63388ed9d0911d7811db4eb1df074978d99cc
Vicko Effendy <vic.eff3@gmail.com>
2020-12-19 21:18:47 +0700

    First time push

---
+++ layouts/Navbar/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <CGrid
+      d="flex"
+      position="sticky"
+      w="100vw"
+      h="80px"
+      p="1rem"
+      flex-dir="column"
+      justify-content="center"
+      column="2"
+    >
+      <CBox w="10%">
+        <CButton mr="3" variant="link"> Bantu Kami </CButton>
+      </CBox>
+      <CBox w="10%">
+        <CIconButton
+          mr="3"
+          :icon="colorMode === 'light' ? 'moon' : 'sun'"
+          :aria-label="`Switch to ${
+            colorMode === 'light' ? 'dark' : 'light'
+          } mode`"
+          @click="$toggleColorMode"
+        />
+      </CBox>
+    </CGrid>
+  </div>
+</template>
+
+<script lang="js">
+import {
+  CGrid,
+  CBox,
+  CButton,
+  CIconButton
+} from '@chakra-ui/vue';
+
+export default {
+  name: 'Navbar',
+  inject: ['$toggleColorMode'],
+  components: {
+    CGrid,
+    CBox,
+    CButton,
+    CIconButton
+  },
+  computed: {
+    toggleColorMode () {
+      return this.$toggleColorMode
+    }
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <CBox v-bind="mainStyles[colorMode]">
+    <Navbar />
+  </CBox>
+</template>
+
+<script lang="js">
+import { CBox } from '@chakra-ui/vue';
+import Navbar from '../layouts/Navbar';
+
+export default {
+  name: 'App',
+  inject: ['$chakraColorMode'],
+  components: {
+    Navbar,
+    CBox,
+  },
+  data () {
+    return {
+      showModal: false,
+      mainStyles: {
+        dark: {
+          bg: 'gray.700',
+          color: 'whiteAlpha.900'
+        },
+        light: {
+          bg: 'white',
+          color: 'gray.900'
+        }
+      }
+    }
+  },
+  computed: {
+    colorMode () {
+      return this.$chakraColorMode()
+    },
+    theme () {
+      return this.$chakraTheme()
+    },
+  },
+}
+</script>
